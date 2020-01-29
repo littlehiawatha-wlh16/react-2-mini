1b4bf6f2884f015604a6426708ab5d1d2b06656b
Jacob Hall <halljaco@yahoo.com>
2020-01-29 15:28:18 -0700

    morningLecture work 3.3.a

--- src/App.js
+++ src/App.js
@@ -9,23 +9,56 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor(props){
+    super(props)
+
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
+    this.updateColor = this.updateColor.bind(this)
+    this.updateFamily = this.updateFamily.bind(this)
+    // this.updateSize = this.updateSize.bind(this)
+  }
+
 
   // updateColor
+  updateColor(val) {
+    console.log('hit', val)
+    this.setState({fontColor:val})
+  }
 
   // updateSize
+  updateSize(val){
+    this.setState({ fontSize: val})
+  }
 
   // updateFamily
+  updateFamily(val){
+    this.setState({fontFamily: val})
+  }
+  
 
   // updateEditStatus
+
+  updateEditStatus(val){
+    this.setState({allowEdit: val})
+  }
 
   render() {
     return (
       <div>
         <div className="headerBar">
           {/* Render EditToggle */}
+          <EditToggle update = {this.updateEditStatus}/>
           {/* Render ColorChanger */}
+          <ColorChanger update = {this.updateColor}/>
           {/* Render SizeChanger */}
+          <SizeChanger update = {this.updateSize} allowEdit = {this.state.allowEdit}/>
           {/* Render FamilyChanger */}
+          <FamilyChangerr/>
         </div>
         <div className="textArea">{/* Render TextContainer */}</div>
       </div>
